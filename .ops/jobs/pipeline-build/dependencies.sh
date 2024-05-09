ebbb99e8e21254ce1f8d4a216548227bc75bcb1d
Brett <brett@cto.ai>
2024-05-09 17:49:57 -0300

    AI Bot Test

--- .ops/jobs/pipeline-build/dependencies.sh
+++ .ops/jobs/pipeline-build/dependencies.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 apt-get update && apt-get -y install git && rm -r /var/cache/apt/archives/
+apt-get update && apt-get -y install npm && rm -r /var/cache/apt/archives/
--- .ops/jobs/pipeline-build/main.sh
+++ .ops/jobs/pipeline-build/main.sh
@@ -4,8 +4,6 @@
     IFS=$'
 	'
 
-    echo $DOCKER_REGISTRY_PASS | docker login -u $DOCKER_REGISTRY_USER --password-stdin $DOCKER_REGISTRY
-git clone https://github.com/$GITHUB_ORG/$GITHUB_REPO && cd $GITHUB_REPO
-docker build -t $DOCKER_REGISTRY/$GITHUB_ORG/$GITHUB_REPO:latest .
-docker push $DOCKER_REGISTRY/$GITHUB_ORG/$GITHUB_REPO:latest
+    npm i
+npm run test
     
